72a2e9a3fc41a26eeca22778f15bd36193383008
Lewis Robbins <lewis.robbins2@gmail.com>
2021-11-25 22:48:28 +0000

    feat: introduce user/name method

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello Wold!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}'.format(name)
