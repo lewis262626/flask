f06874b40221d001ac99e214c2a979f5166b89bf
Lewis Robbins <lewis.robbins2@gmail.com>
2021-11-27 00:18:40 +0000

    Add Moment js

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
